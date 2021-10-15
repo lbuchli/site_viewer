fb94982258a0a7dead8b4b20a89d85fafc70021e
Fabio Zahner <zahnerfabio@gmail.com>
2021-10-15 14:29:50 +0200

    fixed dockerfile

--- site_viewer/start_dev.sh
+++ site_viewer/start_dev.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 if [ "$(cat /usr/src/cert/server-dev.crt | openssl x509 -noout -checkend 3600)" == "Certificate will expire" ]; then
     openssl x509 -req -days 365 -in /usr/src/cert/server-dev.csr -signkey /usr/src/cert/server-dev.key -out /usr/src/cert/server-dev.crt
 fi
