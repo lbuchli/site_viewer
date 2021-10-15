26e877be313ca46d467235040b334d28b2ef612a
Lukas Buchli <lukas.buchli@ost.ch>
2021-10-15 19:13:16 +0200

    WIP

--- site_viewer/static/js/util.js
+++ site_viewer/static/js/util.js
@@ -1,22 +1,24 @@
 import * as THREE from "https://unpkg.com/three@0.126.0/build/three.module.js";
 import { GLTFLoader } from "https://unpkg.com/three@0.126.0/examples/jsm/loaders/GLTFLoader.js";
 
-import * as THREE from "https://unpkg.com/three@0.126.0/build/three.module.js";
-import { GLTFLoader } from "https://unpkg.com/three@0.126.0/examples/jsm/loaders/GLTFLoader.js";
-
 class GLCanvas {
 
-  async constructor(gl) {
+  constructor(canvas, session, referenceSpace, viewerSpace, hitTestSource) {
+    this.gl = canvas.getContext("webgl", {xrCompatible: true});
+    this.session = session;
+    this.referenceSpace = referenceSpace;
+    this.viewerSpace = viewerSpace;
+    this.hitTestSource = hitTestSource;
     this.scene = new THREE.Scene();
     this.gltfloader = new GLTFLoader();
-    this.reference = Matrix4();
+    this.reference = new THREE.Matrix4();
     this.drawers = [];
 
     this.renderer = new THREE.WebGLRenderer({
         alpha: true,
         preserveDrawingBuffer: true,
         canvas: canvas,
-        context: gl
+        context: this.gl
     });
     this.renderer.autoClear = false;
 
@@ -24,21 +26,11 @@ class GLCanvas {
     this.camera.matrixAutoUpdate = false;
 
 
-    this.session = await navigator.xr.requestSession("immersive-ar", {requiredFeatures: ['hit-test']});
     this.session.updateRenderState({
-      baseLayer: new XRWebGLLayer(this.session, gl)
+      baseLayer: new XRWebGLLayer(this.session, this.gl)
     });
 
-    // A 'local' reference space has a native origin that is located
-    // near the viewer's position at the time the session was created.
-    this.referenceSpace = await this.session.requestReferenceSpace('local');
-
-    // Create another XRReferenceSpace that has the viewer as the origin.
-    this.viewerSpace = await this.session.requestReferenceSpace('viewer');
-    // Perform hit testing using the viewer as origin.
-    this.hitTestSource = await this.session.requestHitTestSource({ space: this.viewerSpace });
-
-    let baseDrawF = function (time, frame) {
+    let baseDrawF = (time, frame) => {
         // Queue up the next draw request.
         session.requestAnimationFrame(this.draw);
 
@@ -63,22 +55,31 @@ class GLCanvas {
     }
   }
 
-  addGLTF(path, transform = THREE.Matrix4()) {
-    this.gltfloader.load(path, function(gltf) {
+  static async create(canvas) {
+    let session = await navigator.xr.requestSession("immersive-ar", {requiredFeatures: ['hit-test']});
+    let referenceSpace = await session.requestReferenceSpace('local');
+    let viewerSpace = await session.requestReferenceSpace('viewer');
+    let hitTestSource = await session.requestHitTestSource({ space: viewerSpace });
+
+    return new GLCanvas(canvas, session, referenceSpace, viewerSpace, hitTestSource);
+  }
+
+  addGLTF(path, transform = new THREE.Matrix4()) {
+    this.gltfloader.load(path, (gltf) => {
       let object = gltf.scene;
       object.matrix = transform; // TODO multiply with reference
       object.visible = false;
-      scene.add(object);
+      this.scene.add(object);
     })
   }
 
   selectReference(iconpath, refcallback) {
-    gltfloader.load(iconpath, function(gltf) {
+    gltfloader.load(iconpath, (gltf) => {
       let cursor = gltf.scene;
       cursor.visible = false;
       scene.add(cursor);
 
-      let drawf = function (time, frame) {
+      let drawf = (time, frame) => {
         const hitTestResults = frame.getHitTestResults(hitTestSource);
         if (hitTestResults.length > 0 && cursor) {
             const hitPose = hitTestResults[0].getPose(referenceSpace);
@@ -102,9 +103,9 @@ class GLCanvas {
   }
 
   draw(time, frame) {
-    for (drawf : this.drawers) {
+    this.drawers.forEach((drawf) => {
       drawf(time, frame);
-    }
+    });
     renderer.render(this.scene, this.camera);
   }
 }
